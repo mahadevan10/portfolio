76f4cce8a5e1252af3722cf2e266b4c211e9e8dc
mahadevan10 <mahadevanmn10@gmail.com>
2025-09-12 13:25:05 +0530

    fix: wrap dotenv for production

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 // server.js
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
